9b51095b55a43153d21366822fff446bd7a91273
Lola <lolapaulinebach@gmail.com>
2019-07-17 22:16:45 +0300

    fixed small stylings

---
+++ .history/js/rav-ur-kav_20190717221445.js
@@ -0,0 +1,46 @@
+console.log("step1");
+
+$(document).ready(() => {
+    // Globals
+
+    // Modal buttons
+    let howToButton = $("#how-to");
+    let closeModalButton = $("#close");
+    $(closeModalButton).on("click", () => {
+      $("#modal-wrapper").toggle();
+    });
+    $(howToButton).on("click", () => {
+      $("#modal-wrapper").toggle();
+    });
+    // Rav my Kav button
+    let reccomendedPlanButton = $("#rav-kav-number-button");
+    $(reccomendedPlanButton).on("click", () => {
+        $("#recommended-plan").css("display", "flex");
+    })
+
+});
+console.log("whats up")
+
+
+
+$('#rav-kav-number').on('input', function (event) { 
+    this.value = this.value.replace(/[^0-9]/g, '');
+});
+
+$('.carousel').carousel({
+    interval: false
+  }).on('slid.bs.carousel', function () {
+      curSlide = $('.active');
+    if(curSlide.is( ':first-child' )) {
+       $('.left').hide();
+       return;
+    } else {
+       $('.left').show();   
+    }
+    if (curSlide.is( ':last-child' )) {
+       $('.right').hide();
+       return;
+    } else {
+       $('.right').show();      
+    }
+  });
