823d37fd828bfa3f29f1711a56b0234f9b39a7fb
ykk <yurikestenberg@hotmail.com>
2019-07-17 14:55:55 +0300

    Repairments

--- PricesGushDan.py
+++ PricesGushDan.py
@@ -1,36 +1,37 @@
 """Hardcoded price table for different packages in Gush Dan"""
 
-{141: {name:
-           'גוש דן', 'areas': ['1'], 'monthly': 213, 'weekly': 64, 'daily': 13.5},
- 142: {name:
-           'גוש דן מורחב', 'areas': [1, 2.1, 2.2, 2.3], 'monthly': 243, 'weekly': 94, 'daily': 21.5},
- 148: {name:
-           'המטרופולין גוש דן', 'areas': [1, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3, 3.4], 'monthly': 423, 'weekly': 162.5,
-       'daily': 37.5},
- 143: {name:
-           'גוש דן צפון', 'areas': [1, 2.1, 2.2, 3.1, 3.4], 'monthly': 299, 'weekly': 115.5, 'daily': 26.5},
- 144: {name:
-           'גוש דן דרום', 'areas': [1 + 2.2 2.3 + 3.2 3.3], 'monthly': 299, 'weekly': 115.5, 'daily': 26.5},
- 152: {name:
-           'שרון', 'areas': [2.1], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 145: {name:
-           'השרון ונתניה', 'areas': [2.1 + 3.1 3.4], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
- 153: {name:
-           'ראש העין שוהם', 'areas': [2.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 146: {name:
-           "מטרופולין מזרחי", 'areas': [2.2 + 3.2 3.4], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
- 154: {name:
-           "רחובות", 'areas': [2.3], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 147: {name:
-           "מטרופולין דרומי", 'areas': [2.3 + 3.2 3.3], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
- 155: {name:
-           "נתניה", 'areas': [3.1], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 156: {name:
-           "חבל מודיעין", 'areas': [3.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 697: {name:
-           "חבל מודיעין", 'areas': [3.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 157: {name:
-           'אשדוד', 'areas': [3.3], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
- 753: {name:
-           'השומרון', 'areas': [3.4], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5}
- }
+prices = {141: {'name':
+                    'גוש דן', 'areas': [1], 'monthly': 213, 'weekly': 64, 'daily': 13.5},
+          142: {'name':
+                    'גוש דן מורחב', 'areas': [1, 2.1, 2.2, 2.3], 'monthly': 243, 'weekly': 94, 'daily': 21.5},
+          148: {'name':
+                    'המטרופולין גוש דן', 'areas': [1, 2.1, 2.2, 2.3, 3.1, 3.2, 3.3, 3.4], 'monthly': 423,
+                'weekly': 162.5,
+                'daily': 37.5},
+          143: {'name':
+                    'גוש דן צפון', 'areas': [1, 2.1, 2.2, 3.1, 3.4], 'monthly': 299, 'weekly': 115.5, 'daily': 26.5},
+          144: {'name':
+                    'גוש דן דרום', 'areas': [1, 2.2, 2.3, 3.2, 3.3], 'monthly': 299, 'weekly': 115.5, 'daily': 26.5},
+          152: {'name':
+                    'שרון', 'areas': [2.1], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          145: {'name':
+                    'השרון ונתניה', 'areas': [2.1, 3.1, 3.4], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
+          153: {'name':
+                    'ראש העין שוהם', 'areas': [2.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          146: {'name':
+                    "מטרופולין מזרחי", 'areas': [2.2, 3.2, 3.4], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
+          154: {'name':
+                    "רחובות", 'areas': [2.3], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          147: {'name':
+                    "מטרופולין דרומי", 'areas': [2.3, 3.2, 3.3], 'monthly': 213, 'weekly': 82, 'daily': 21.5},
+          155: {'name':
+                    "נתניה", 'areas': [3.1], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          156: {'name':
+                    "חבל מודיעין", 'areas': [3.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          697: {'name':
+                    "חבל מודיעין", 'areas': [3.2], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          157: {'name':
+                    'אשדוד', 'areas': [3.3], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5},
+          753: {'name':
+                    'השומרון', 'areas': [3.4], 'monthly': 149, 'weekly': 57.5, 'daily': 13.5}
+          }
