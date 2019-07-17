bbe9e2fa32dc7205c8b723488f170a070dcabb2e
ykk <yurikestenberg@hotmail.com>
2019-07-18 00:00:19 +0300

    extracted the cities in hebrew

--- tablegathering.py
+++ tablegathering.py
@@ -6,7 +6,9 @@ import numpy as np
 from user_generator import UserRandomizer
 from area_dict import gush_dan
 from PricesGushDan import prices
+import re
 
+pattern_re = re.compile(r'/עיר:\s(.+)\sרציף:/')
 pd.set_option('display.max_columns', 15)
 
 user_generator = UserRandomizer(a)
@@ -20,10 +22,15 @@ cities_per_area = pd.DataFrame.from_dict(gush_dan, orient='index').transpose()
 
 stops = pd.read_csv(r'stops.csv')
 stops.drop(['stop_id', 'stop_name', 'location_type', 'parent_station', 'zone_id'], axis='columns', inplace=True)
+stops.drop(13755, inplace=True)
+stops['city'] = [' '.join(x.split(':')[2].split()[:-1]) for x in stops['stop_desc'].values]  # TODO: Extracted the cities here
+# print(stops['stop_desc'].values[0])
+# print(pattern_re.match(stops['stop_desc'].values[0]))
 # print(stops.head())
 
 price_df = pd.DataFrame.from_dict(prices, orient='index')
 # print(price_df.head())
 
-print(stops['stop_code'].isin(passenger['Place'].values))  # TODO: FOUND THE STOPS
+
+# print(stops[stops['stop_code'].isin(passenger['Place'].values)])  # TODO: FOUND THE STOPS
 
