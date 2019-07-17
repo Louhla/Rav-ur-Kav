7f70df22fdcb83138550db6264544be43a9f98d3
ykk <yurikestenberg@hotmail.com>
2019-07-18 00:16:49 +0300

    added cities to passenger table

--- tablegathering.py
+++ tablegathering.py
@@ -6,9 +6,7 @@ import numpy as np
 from user_generator import UserRandomizer
 from area_dict import gush_dan
 from PricesGushDan import prices
-import re
 
-pattern_re = re.compile(r'/עיר:\s(.+)\sרציף:/')
 pd.set_option('display.max_columns', 15)
 
 user_generator = UserRandomizer(a)
@@ -23,14 +21,14 @@ cities_per_area = pd.DataFrame.from_dict(gush_dan, orient='index').transpose()
 stops = pd.read_csv(r'stops.csv')
 stops.drop(['stop_id', 'stop_name', 'location_type', 'parent_station', 'zone_id'], axis='columns', inplace=True)
 stops.drop(13755, inplace=True)
-stops['city'] = [' '.join(x.split(':')[2].split()[:-1]) for x in stops['stop_desc'].values]  # TODO: Extracted the cities here
-# print(stops['stop_desc'].values[0])
-# print(pattern_re.match(stops['stop_desc'].values[0]))
+stops['city'] = [' '.join(x.split(':')[2].split()[:-1]) for x in stops['stop_desc'].values]  # Extracted the cities here
 # print(stops.head())
 
 price_df = pd.DataFrame.from_dict(prices, orient='index')
 # print(price_df.head())
 
+translation_df = stops[stops['stop_code'].isin(passenger['Place'].values)].loc[:,['stop_code','city']]  # FOUND THE STOPS
+passenger = pd.merge(passenger, translation_df, left_on=['Place'], right_on=['stop_code'])  # joined the city data to df
+print(passenger)
 
-# print(stops[stops['stop_code'].isin(passenger['Place'].values)])  # TODO: FOUND THE STOPS
 
