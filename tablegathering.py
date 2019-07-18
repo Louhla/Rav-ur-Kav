399f034f398f5e286393786e20ed11b63196b3b1
ykk <yurikestenberg@hotmail.com>
2019-07-18 11:44:33 +0300

    generating table images

--- tablegathering.py
+++ tablegathering.py
@@ -8,6 +8,8 @@ from area_dict import gush_dan
 from PricesGushDan import prices
 import seaborn as sns
 import matplotlib.pyplot as plt
+import six
+
 
 pd.set_option('display.max_columns', 15)
 
@@ -44,4 +46,36 @@ for row, city in enumerate(passenger['city'].values):
 
 passenger['region'] = areas
 
-print(passenger)
+# sns.set()
+# sns.barplot(passenger['region'].value_counts().transpose())
+# plt.show()
+
+print(passenger['region'].value_counts())
+
+
+def render_mpl_table(data, filename,col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0,
+                     ax=None, **kwargs):
+    if ax is None:
+        size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+        fig, ax = plt.subplots(figsize=size)
+        ax.axis('off')
+
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs)
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in  six.iteritems(mpl_table._cells):
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    fig = ax.get_figure()
+    fig.savefig(filename)
+    pass
+
+render_mpl_table(passenger, 'random_passengertable.png', header_columns=0, col_width=2.0)
